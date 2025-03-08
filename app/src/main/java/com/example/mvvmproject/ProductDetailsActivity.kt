bb160131a37c9026759f021f0ca7501650ddfb39
sande <sandeeppatel7762@gmail.com>
2025-03-08 15:29:00 +0530

    new update activity

--- app/src/main/java/com/example/mvvmproject/ProductDetailsActivity.kt
+++ app/src/main/java/com/example/mvvmproject/ProductDetailsActivity.kt
@@ -10,7 +10,7 @@ class ProductDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_addcard)
+        setContentView(R.layout.activity_product_details)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
